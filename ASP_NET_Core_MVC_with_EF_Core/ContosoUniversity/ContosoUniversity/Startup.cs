966b7d04dc98b99ad72e3b4c0bdcb80dab41f901
BC-AOrtiz <alberto.ortiz@bigcommerce.com>
2024-03-15 13:32:29 -0600

    Inital MVC course commit

---
+++ ASP_NET_Core_MVC_with_EF_Core/ContosoUniversity/ContosoUniversity/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		public void ConfigurationServices(IServiceCollection services)
+		{
+			services.AddDbContext<SchoolContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+			services.AddControllersWithViews();
+		}
+	}
+}
+
