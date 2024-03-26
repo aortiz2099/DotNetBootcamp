219e4402735b2ff8f93f6adcc9d41c68990b3fbe
BC-AOrtiz <alberto.ortiz@bigcommerce.com>
2024-03-26 15:55:59 -0600

    Add Department, Instructors, Courses controllers

--- ASP_NET_Core_MVC_with_EF_Core/ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
+++ ASP_NET_Core_MVC_with_EF_Core/ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
@@ -20,11 +20,53 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Students
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(
+            string sortOrder,
+            string currentFilter,
+            string searchString,
+            int? pageNumber)
         {
-              return _context.Students != null ? 
-                          View(await _context.Students.ToListAsync()) :
-                          Problem("Entity set 'SchoolContext.Students'  is null.");
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if(searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+
+            var students = from s in _context.Students
+                           select s;
+            if(!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.Contains(searchString)
+                || s.FirstMidName.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            int pageSize = 3;
+            return View(await PaginatedList<Student>.CreateAsync(students.AsNoTracking(),pageNumber ?? 1, pageSize));
         }
 
         // GET: Students/Details/5
@@ -133,7 +175,7 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Students/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
         {
             if (id == null || _context.Students == null)
             {
@@ -141,10 +183,18 @@ namespace ContosoUniversity.Controllers
             }
 
             var student = await _context.Students
+                .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.ID == id);
             if (student == null)
             {
                 return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Delete failed. Try again, and if the problem persists " +
+                    "See your system administrator.";
             }
 
             return View(student);
@@ -155,18 +205,22 @@ namespace ContosoUniversity.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Students == null)
-            {
-                return Problem("Entity set 'SchoolContext.Students'  is null.");
-            }
             var student = await _context.Students.FindAsync(id);
-            if (student != null)
+            if(student == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            try
             {
                 _context.Students.Remove(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
             }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            catch (DbUpdateException )
+            {
+                return RedirectToAction(nameof(Delete), new { id= id, saveChangesError = true });
+            }
         }
 
         private bool StudentExists(int id)
--- ASP_NET_Core_MVC_with_EF_Core/ContosoUniversity/ContosoUniversity/Models/Course.cs
+++ ASP_NET_Core_MVC_with_EF_Core/ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -1,17 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace ContosoUniversity.Models
 {
 	public class Course
 	{
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
         public int CourseID { get; set; }
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+        [Range(0, 5)]
         public int Credits { get; set; }
+        public int DepartmentID { get; set; }
 
+        [ValidateNever]
+        public Department Department { get; set; }
+        [ValidateNever]
         public ICollection<Enrollment> Enrollments { get; set; }
-
+        [ValidateNever]
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
     }
 }
 
